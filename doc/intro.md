a761a5c53c35d32671ccbf4873c1dfdf6936ec30
Alex Chythlook <achythlook@gmail.com>
2018-07-10 21:23:12 -0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dslearn
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dslearn/core_test.clj
@@ -0,0 +1,7 @@
+(ns dslearn.core-test
+  (:require [clojure.test :refer :all]
+            [dslearn.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
